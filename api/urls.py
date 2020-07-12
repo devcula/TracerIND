38eb67e4c0f56a63bf2b9aee7a85327c701b60ce
Kaneki <kaneki@aogiridevs.tech>
2020-07-13 00:26:29 +0530

    Models and URL fix

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from api import urls as API
+from . import views
+
+#CRUD Functionality First, API For ANDROID Next and then API for GettingData and Filtering Data
+
+urlpatterns = [
+    path('',views.APIView,name = "APIList"), 
+    path('AddPatient/',views.AddPatient,name = "AddPatient"),
+    path('DeletePatient/',views.DeletePatient,name = "DeletePatient"),
+    path('UpdatePatient/',views.UpdatePatient,name = "UpdatePatient"),
+    path('GetPatient/',views.GetPatient,name = "GetPatient"),
+
+]
--- village/models.py
+++ village/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
 import time
-from village_sec import models as v_s
+from village_sec.models import Village_sec
 
 
 class Village(models.Model):
     village_id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=50)
-    village_sec = models.ForeignKey(v_s.Village_sec, on_delete=models.CASCADE)
+    village_sec = models.ForeignKey(Village_sec, on_delete=models.CASCADE)
