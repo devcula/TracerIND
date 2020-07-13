551f60ed10a6760df491735f6b59681754e85b8e
roygrv21 <gauravroy99@gmail.com>
2020-07-13 23:41:34 +0530

    single page form

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import Anemia from './Anemia';
+import Grid from '@material-ui/core/Grid';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Test
-    </div>
+    <Grid container direction='column'>
+    <Grid item>
+    <Header/>
+    </Grid>
+    <Grid item container>
+    <Grid item xs={3}/>
+    <Anemia />
+    <Grid item xs={3}/>
+    
+    </Grid>
+    
+        </Grid>
+
   );
 }
 
