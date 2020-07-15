f30a68f45b3fab7ab3c70c505672162027c57562
Kaneki <kaneki@aogiridevs.tech>
2020-07-15 18:52:57 +0530

    added all blank fields and/or default fields

--- patient/models.py
+++ patient/models.py
@@ -28,13 +28,13 @@ class Patient (models.Model):
     PVGT = models.CharField(default=None, max_length=3)
 
     dateoftesting = models.DateField()
-    serumCreatinine = models.DecimalField(max_digits=5, decimal_places=1)
-    bloodUrea = models.DecimalField(max_digits=5, decimal_places=1)
-    uricAcid = models.DecimalField(max_digits=5, decimal_places=1)
-    electrolytes_sodium = models.DecimalField(max_digits=5, decimal_places=1)
+    serumCreatinine = models.DecimalField(max_digits=5, decimal_places=1,blank = True)
+    bloodUrea = models.DecimalField(max_digits=5, decimal_places=1,blank = True)
+    uricAcid = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
+    electrolytes_sodium = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
     electrolytes_potassium = models.DecimalField(
-        max_digits=5, decimal_places=1)
-    bun = models.DecimalField(max_digits=5, decimal_places=1)
+        max_digits=5, decimal_places=1,blank = True)
+    bun = models.DecimalField(max_digits=5, decimal_places=1,blank=True)
 
     pedalEdema = models.CharField(max_length=2)
     # if above is yes then ask single/bilateral
@@ -43,26 +43,26 @@ class Patient (models.Model):
     # good/abnormal
     kidneystatus = models.CharField(max_length=50)
     # in case abnormal ask following
-    ailments = models.TextField(max_length=100)
-    dialysis = models.BooleanField()
-    doctorreq = models.BooleanField()
+    ailments = models.TextField(max_length=100,blank=True)
+    dialysis = models.BooleanField(default = False)
+    doctorreq = models.BooleanField(default = False)
     # if above is yes then ask refer to the following hospital
-    hospitalAdmit = models.CharField(max_length=50)
+    hospitalAdmit = models.CharField(max_length=50,blank = True)
 
-    dateOfAdmit = models.DateField(auto_now=False, auto_now_add=False)
-    refered = models.BooleanField()
+    dateOfAdmit = models.DateField(auto_now=False, auto_now_add=False,blank = True)
+    refered = models.BooleanField(default = False)
     # case:yes
-    referredto = models.CharField(max_length=50)
-    status = models.TextField(max_length=300)
-    treatmentDone = models.TextField(max_length=300)
+    referredto = models.CharField(max_length=50,blank = True)
+    status = models.TextField(max_length=300,blank=True)
+    treatmentDone = models.TextField(max_length=300,blank=True)
     discharge = models.DateField(auto_now=False, auto_now_add=False)
-    dischargeStatus = models.TextField(max_length=500)
+    dischargeStatus = models.TextField(max_length=500,blank=True)
     # case:no
     # treatmentDone
     # discharge
     # dischargeStatus
-    deceased = models.BooleanField()
+    deceased = models.BooleanField(default = False)
     # if above is answered yes
     deathDate = models.DateField(auto_now=False, auto_now_add=False)
-    placeOfDeath = models.CharField(max_length=50)
-    causeOfDeath = models.TextField(max_length=300)
+    placeOfDeath = models.CharField(max_length=50,blank=True)
+    causeOfDeath = models.TextField(max_length=300,blank=True)
