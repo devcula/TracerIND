185804ef7af10b6ca568ddc08fb95b9761876eb3
Kaneki <kaneki@aogiridevs.tech>
2020-07-17 18:43:22 +0530

    GetVillageName API up

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('GetVillageSecData/',views.GetVillageSecData,name = "GetVillageSecData"),
     path('GetVillageData/',views.GetVillageData,name = "GetVillageData"),
     path('GetPatientData_Village/',views.GetPatientData_Village,name = "GetPatientData_Village"),
-
+     path('GetVIllageName/',views.GetVIllageName,name = "GetVIllageName"),
     #ANDROID API
     path('DroidDump/',views.DroidDump,name = "DroidDump"),
 
