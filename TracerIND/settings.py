82c2759ec975d30b70c2b49a2f98e63ad409b6e9
Kaneki <kaneki@aogiridevs.tech>
2020-07-17 16:01:32 +0530

    REACT

--- TracerIND/settings.py
+++ TracerIND/settings.py
@@ -46,10 +46,13 @@ INSTALLED_APPS = [
     'village',
     'village_sec',
     'frontend',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # added to solve CORS
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,7 +67,7 @@ ROOT_URLCONF = 'TracerIND.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True 
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'frontend/client/build/index.html')
+    return render(request,'client/build/index.html')
