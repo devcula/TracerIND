b4c6032a9b209f3e5bebe5faf2b4372cce7a7633
roygrv21 <gauravroy99@gmail.com>
2020-07-26 21:23:41 +0530

    Adding 404 page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import MainForm from './components/MainForm/MainForm';
 import Directory from './components/Directory/Directory';
-
+import Error404 from './components/Error404/Error404';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Header />
+        <Header />
+          <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/add" component={MainForm} />
-          <Route path="/directory" component={Directory} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/add" component={MainForm} />
+          <Route exact path="/directory" component={Directory} />
+          <Route component={Error404} />
+          </Switch>
         </div>
       </Router>
     );
