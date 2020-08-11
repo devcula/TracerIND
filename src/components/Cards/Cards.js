e8215beda3d62337dc4fc16f32aa97b9c97f73c3
Abhishek Prasad <devcula@outlook.com>
2020-08-11 22:22:35 +0530

    Code cleanup

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,88 +1,86 @@
 import React from 'react';
 import './Cards.css'
-import {Card,CardDeck} from 'react-bootstrap';
+import { Card, CardDeck } from 'react-bootstrap';
 import CountUp from 'react-countup';
-// import cx from 'classnames';
 
+const Cards = ({ data: { confirmed, recovered, deaths, active } }) => {
+  if (!confirmed) {
+    return <h1>Loading...</h1>
+  }
 
-const Cards= ({data:{confirmed,recovered,deaths,active}}) => {
-    if(!confirmed){
-        return <h1>Loading...</h1>
-    }
-
-    return(
-        <div className="container">
-          <h1>
-            COVID-19 stats for Andhra Pradesh
+  return (
+    <div className="container">
+      <h1>
+        COVID-19 stats for Andhra Pradesh
           </h1>
-        <CardDeck>
+      <CardDeck>
         <Card
-            style={{ width: '18rem'}}
-            text="D13C2B"
-            bg='light'
-            xs={12}
-            s={6}
-            md={3}
-            className="mb-2"
-          >
+          style={{ width: '18rem' }}
+          text="D13C2B"
+          bg='light'
+          xs={12}
+          s={6}
+          md={3}
+          className="mb-2"
+        >
 
-            <Card.Body  className="confirmed">
-              <Card.Title> Confirmed </Card.Title>
-              <Card.Text><CountUp start={0} end={confirmed} duration={1.75} separator=","/> </Card.Text>
+          <Card.Body className="confirmed">
+            <Card.Title> Confirmed </Card.Title>
+            <Card.Text><CountUp start={0} end={confirmed} duration={1.75} separator="," /> </Card.Text>
 
-            </Card.Body>
-          </Card>
-          <Card
-              style={{ width: '18rem' }}
-              bg='light'
-              className="mb-2"
-              xs={12}
-              s={6}
-              md={3}
-            >
+          </Card.Body>
+        </Card>
+        <Card
+          style={{ width: '18rem' }}
+          bg='light'
+          className="mb-2"
+          xs={12}
+          s={6}
+          md={3}
+        >
 
-              <Card.Body  className="active">
-                <Card.Title> Active </Card.Title>
-                <Card.Text> <CountUp start={0} end={active} duration={1.75} separator=","/></Card.Text>
+          <Card.Body className="active">
+            <Card.Title> Active </Card.Title>
+            <Card.Text> <CountUp start={0} end={active} duration={1.75} separator="," /></Card.Text>
 
-              </Card.Body>
-            </Card>
-          <Card
-              style={{ width: '18rem' }}
-              bg='light'
-              xs={12}
-              s={6}
-              md={3}
-              className="mb-2"
-            >
+          </Card.Body>
+        </Card>
+        <Card
+          style={{ width: '18rem' }}
+          bg='light'
+          xs={12}
+          s={6}
+          md={3}
+          className="mb-2"
+        >
 
-              <Card.Body className="recovered">
-                <Card.Title> Recovered </Card.Title>
-                <Card.Text> <CountUp start={0} end={recovered} duration={1.75} separator=","/></Card.Text>
+          <Card.Body className="recovered">
+            <Card.Title> Recovered </Card.Title>
+            <Card.Text> <CountUp start={0} end={recovered} duration={1.75} separator="," /></Card.Text>
 
-              </Card.Body>
-            </Card>
-            <Card
-                style={{ width: '18rem'}}
-                bg='light'
-                className="mb-2"
-                xs={12}
-                s={6}
-                md={3}
-              >
+          </Card.Body>
+        </Card>
+        <Card
+          style={{ width: '18rem' }}
+          bg='light'
+          className="mb-2"
+          xs={12}
+          s={6}
+          md={3}
+        >
 
-                <Card.Body className="deaths">
-                  <Card.Title> Deaths </Card.Title>
-                  <Card.Text> <CountUp start={0} end={deaths} duration={1.75} separator=","/></Card.Text>
+          <Card.Body className="deaths">
+            <Card.Title> Deaths </Card.Title>
+            <Card.Text> <CountUp start={0} end={deaths} duration={1.75} separator="," /></Card.Text>
 
-                </Card.Body>
-              </Card>
+          </Card.Body>
+        </Card>
 
 
-              </CardDeck>
+      </CardDeck>
 
-        </div>
-        )
+    </div>
+  )
 }
 
 export default Cards;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react'
-import { Jumbotron, Container, Row} from 'react-bootstrap';
+import { Jumbotron, Container, Row } from 'react-bootstrap';
 import './Home.css';
-// import Circle from '../CircularProgressBar/CircularProgressBar';
 import Cards from '../Cards/Cards';
 import { fetchData } from '../../CovidApi';
 
 export default class Home extends Component {
   state = {
     data: {},
-}
+  }
 
-async componentDidMount(){
-  const fetchedData = await fetchData();
-  this.setState({ data:fetchedData })
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData })
 
-}
-    render() {
-      const {data} = this.state;
-        return (
-            <Container>
-                <Jumbotron>
-                    <h2>Welcome to TracerIND</h2>
-                </Jumbotron>
-                <Row className="show-grid text-center">
-                   <Cards data={ data }/>
-                </Row>
-            </Container>
-        )
-    }
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <Container>
+        <Jumbotron>
+          <h2>Welcome to TracerIND</h2>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Cards data={data} />
+        </Row>
+      </Container>
+    )
+  }
 }
