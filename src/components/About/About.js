27448d40b19e5f7077e97ca7827e992704baf70b
palakrai000 <68582940+palakrai000@users.noreply.github.com>
2020-08-11 23:38:34 +0530

    <About us page>

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,56 +1,81 @@
 
-import React, { Component } from 'react'
-import { Container, Col, Row } from 'react-bootstrap';
+import React, { Component } from 'react';
+import {  Col, Row} from 'react-bootstrap';
+
+import imgCard1 from "./img/S1.png";
+import imgCard2 from "./img/S2.png";
+import imgCard3 from "./img/S3.png";
+import imgCard4 from "./img/S4.png";
+import imgCard5 from "./img/S5.png";
+import imgCard6 from "./img/S6.png";
+import imgCard7 from "./img/S7.png";
 import './About.css';
 
-export default class About extends Component {
+
+
+ export default class About extends Component {
     render() {
-        return (
-            <Container>
+        return (      
+          <div>
 
-                <fieldset style={{ backgroundImage: "linear-gradient(to right top, #7a9e7e, #3d7b74, #0a5767, #0c314d, #160c28)", color: "white", marginTop: "10%" }}>
-                    <Row style={{padding: "1rem"}}>
-                        <Col xs={12} sm={8} smoffset={2}>
-                            <h3>About TracerIND Team </h3>
-                            <ul>
-                              <li>Leads</li>
-                                  <ul>
-                                  <li>Shlok Parida</li>
-                                  <li>Prakruti Chandak </li>
-                                    <li>Abhishek Prasad</li>
-                                </ul>
-                                 <br/>
-                                  <li>Frontend Developers (ReactJS)</li>
-                                  <ul>
-                                  <li>Abhishek Prasad</li>
-                                  <li>Hrituja Khatavkar</li>
-                                  <li>Gaurav Roy</li>
-
-                                </ul>
-                                <br/>
-
-                                  <li>Android Developers</li>
-                                  <ul>
-                                  <li>Akshat Rawat</li>
-                                  <li>Shubhankar</li>
-                                </ul>
-                                <br/>
-                              <li>Backend Developers(Django)</li>
-                                  <ul>
-                                  <li>Shlok Parida</li>
-                                  <li>Prakruti Chandak </li>
-                                  </ul>
-                                  </ul>
-
-                        </Col>
-                        <Col xs={12} sm={4} className="sidebar-section">
-                            <h4>Contact Us!</h4>
-                            <p>Email: </p>
-                            <p>Phone </p>
-                        </Col>
-                    </Row>
-                </fieldset>
-            </Container >
-        )
-    }
+    <div class="container">
+    <h1 class="heading"><span>meet</span>Our Team</h1>
+      <div class="profiles">
+        <Row>
+         <Col sm="3" md="4">
+           <div class="profile">
+             <img src={imgCard1} class="profile-img"></img>
+               <h3 class="user-name">Prakruti Chandak</h3>
+                 <h5>Team lead</h5>
+             </div>
+           </Col>
+         <Col sm="3" md="4">
+           <div class="profile">  
+             <img src={imgCard7} class="profile-img"></img>
+               <h3 class="user-name">Shubhankar</h3>
+             <h5>Android Developer</h5>
+           </div>
+         </Col>
+        <Col sm="3"md="4">
+          <div class="profile">
+            <img src={imgCard2} class="profile-img"></img>
+              <h3 class="user-name">Abhisekh Prasad </h3>
+                <h5>Team lead</h5>
+           </div>
+          </Col>
+       </Row>
+       <Row>
+         <Col sm="3" md="3">
+           <div class="profile">
+             <img src={imgCard4} class="profile-img"></img>
+               <h3 class="user-name">Hrituja Khatavkar</h3>
+                 <h5>Front-end developer</h5>
+           </div>
+          </Col>
+          <Col sm="3" md="3">
+            <div class="profile">
+              <img src={imgCard5} class="profile-img"></img>
+                <h3 class="user-name">Akshat </h3>
+                  <h5>Android Developer</h5>
+                 </div></Col>
+           <Col sm="3"md="3">
+              <div class="profile">
+                <img src={imgCard6} class="profile-img"></img>                   
+                 <h3 class="user-name">  Gaurav Roy </h3>
+                    <h5>Front-end Developer</h5>
+             </div>
+            </Col>
+            <Col sm="3"md="3">
+              <div class="profile">
+               <img src={imgCard3} class="profile-img"></img>
+                  <h3 class="user-name">Shlok Parida</h3>
+                    <h5>Team lead</h5>
+               </div>
+           </Col>
+         </Row>
+    </div>
+  </div>
+ </div>
+      )
+  }
 }
