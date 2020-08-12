7d46e301fdd62dfd752489ce78fb42b7b5c877bd
Abhishek Prasad <devcula@outlook.com>
2020-08-12 20:00:23 +0530

    Hook check

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import './Home.css';
 import Cards from '../Cards/Cards';
 import { fetchData } from '../../CovidApi';
 
-//Comment
 export default class Home extends Component {
   state = {
     data: {},
