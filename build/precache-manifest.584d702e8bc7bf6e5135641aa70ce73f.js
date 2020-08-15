924b4f0259f3586ce8cd4fb6753831e4b5dc4a3e
Abhishek Prasad <devcula@outlook.com>
2020-08-15 12:19:13 +0530

    Added build folder to git

---
+++ build/precache-manifest.584d702e8bc7bf6e5135641aa70ce73f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbb3c24e8bf35e4175067e39e055073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1cca587a8c968172a9d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4a1c7eabafd8eb18b4e9",
+    "url": "/static/css/main.229b05c6.chunk.css"
+  },
+  {
+    "revision": "e1cca587a8c968172a9d",
+    "url": "/static/js/2.1324555e.chunk.js"
+  },
+  {
+    "revision": "8e1e783d29d63892ae8626521ac815e0",
+    "url": "/static/js/2.1324555e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1c7eabafd8eb18b4e9",
+    "url": "/static/js/main.b7dbbee0.chunk.js"
+  },
+  {
+    "revision": "ac336dfc93cdd79ad3b6",
+    "url": "/static/js/runtime-main.83a9d60b.js"
+  },
+  {
+    "revision": "fa95cb07453b4c3c82664acec8aa96b9",
+    "url": "/static/media/S1.fa95cb07.png"
+  },
+  {
+    "revision": "36374932a9e70c607f1fa8b82524c2fc",
+    "url": "/static/media/S2.36374932.png"
+  },
+  {
+    "revision": "52172d806fcd855ebd1f540369bc735d",
+    "url": "/static/media/S3.52172d80.png"
+  },
+  {
+    "revision": "534dbb4b101b2f090874f2076472e0b1",
+    "url": "/static/media/S4.534dbb4b.png"
+  },
+  {
+    "revision": "50e3b7253abb945b228369c17235505d",
+    "url": "/static/media/S5.50e3b725.png"
+  },
+  {
+    "revision": "0b25fff50fceb63cca250bf7986ebfc7",
+    "url": "/static/media/S6.0b25fff5.png"
+  },
+  {
+    "revision": "3d234977f8a23919fa77160b8bfbf857",
+    "url": "/static/media/S7.3d234977.png"
+  },
+  {
+    "revision": "e386adfe21200878782bc4618489b5ce",
+    "url": "/static/media/logo_transparent.e386adfe.png"
+  }
+]);
