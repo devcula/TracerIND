c00c46e9bd562e669c771cfce935dc7dc4e3afcb
Hrituja <hrkhatavkar@gmail.com>
2020-08-27 14:22:36 +0530

    Carousel Images

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
 
 // import S from './CarouselImg/event.jpg';
-import S1 from './CarouselImg/1.png';
-import S2 from './CarouselImg/2.png';
-import S3 from './CarouselImg/3.png';
-import S4 from './CarouselImg/4.png';
-import S5 from './CarouselImg/5.png';
+import S1 from './CarouselImg/1/1.jpg';
+import S2 from './CarouselImg/2/2.jpg';
+import S3 from './CarouselImg/3/3.jpg';
+import S4 from './CarouselImg/4/4.jpg';
+import S5 from './CarouselImg/5/5.jpg';
 
 
 import './Carousel.css';
@@ -23,39 +23,39 @@ export default class Slider extends Component {
         <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100 h-50"
-            src={S5}
+            className="cImage d-block w-100 h-50"
+            src={S1}
             alt="First slide"
           />
         </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 h-50"
-              src={S1}
+              className="cImage d-block w-100 h-50"
+              src={S2}
               alt="Second slide"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 h-50"
+              className="cImage d-block w-100 h-50"
               width={200}
-              src={S2}
+              src={S3}
               alt="Third slide"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 h-50"
+              className="cImage d-block w-100 h-50"
               width={200}
-              src={S3}
+              src={S4}
               alt="Fourth slide"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 h-50"
+              className="cImage d-block w-100 h-50"
               width={200}
-              src={S4}
+              src={S5}
               alt="Fifth slide"
             />
           </Carousel.Item>
